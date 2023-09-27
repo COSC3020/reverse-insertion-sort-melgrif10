505a377a54aed25338ae3cd43b71604ee0942c8b
melgrif10 <mgriff25@uwyo.edu>
2023-09-27 00:28:42 -0600

    Reverse Insertion Sort

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 function insertionSortReverse(array) {
-    for (var i=array.length-1; i>=0; i--){
-        var val = array[i];
-        var j;
-        for(j=i; j<array.length && array[j+1]<val; j++){
-            array[j]=array[j+1];
+    for (var i=array.length-1; i>=0; i--){  //Have i=the end of the array, make sure i>=0 and decrement i each loop
+        var val = array[i];     //Store the current element
+        var j;  
+        for(j=i; j<array.length && array[j+1]<val; j++){    //Compare the current element with the element that follows. 
+            array[j]=array[j+1];    //Shift if necessary
         }
-        array[j]=val;
+        array[j]=val;   //Place current element in the right spot
     }
     return array;
 }
